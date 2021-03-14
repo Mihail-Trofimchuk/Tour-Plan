28800f3263d4bc18f26f5cf10b5827929c4f073c
Mihail-Trofimchuk <mihatr2002@gmail.com>
2021-03-14 20:32:33 +0300

    create: move and scrole

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,32 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
   keyboard: true,
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+  keyboard: true,
+});
+$(function () {
+  $('.newsletter-parallax').parallax();
+});
+$(document).ready(function () {
+  $(window).bind('scroll', function (e) {
+    var scrolledY = $(window).scrollTop();
+    $('.bgWrapper').css(
+      'background-position',
+      'center -' + scrolledY * 0.2 + 'px'
+    );
+  });
 });
